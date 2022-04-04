131133e83e6222416e6871e99e1b3ffc880c3484
CryptoDogg <d.enkikairo@gmail.com>
2022-04-04 11:35:34 +0900

    refactor

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,54 +1,13 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  Freezed as FreezedEvent,
   Minted as MintedEvent,
-  Transfer as TransferEvent,
   Updated as UpdatedEvent,
-  STokensManager,
 } from "../generated/STokensManager/STokensManager"
 import {
   Lockup
 } from "../generated/STokensManager/Lockup"
 import {
-  Approval,
-  ApprovalForAll,
-  Freezed,
-  Minted,
-  Transfer,
-  Updated,
-  STokenIdAmount,
   TotalAmount,
 } from "../generated/schema"
-
-// export function handleApproval(event: ApprovalEvent): void {
-//   let entity = new Approval(
-//     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-//   )
-//   entity.owner = event.params.owner
-//   entity.approved = event.params.approved
-//   entity.tokenId = event.params.tokenId
-//   entity.save()
-// }
-
-// export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-//   let entity = new ApprovalForAll(
-//     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-//   )
-//   entity.owner = event.params.owner
-//   entity.operator = event.params.operator
-//   entity.approved = event.params.approved
-//   entity.save()
-// }
-
-// export function handleFreezed(event: FreezedEvent): void {
-//   let entity = new Freezed(
-//     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-//   )
-//   entity.tokenId = event.params.tokenId
-//   entity.freezingUser = event.params.freezingUser
-//   entity.save()
-// }
 
 export function handleMinted(event: MintedEvent): void {
   let day = event.block.timestamp.toI32() / 86400
@@ -65,16 +24,6 @@ export function handleMinted(event: MintedEvent): void {
     }
   }
 }
-
-// export function handleTransfer(event: TransferEvent): void {
-//   let entity = new Transfer(
-//     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-//   )
-//   entity.from = event.params.from
-//   entity.to = event.params.to
-//   entity.tokenId = event.params.tokenId
-//   entity.save()
-// }
 
 export function handleUpdated(event: UpdatedEvent): void {
   let day = event.block.timestamp.toI32() / 86400
