9d679995d8219e81845b5fdb99afb589c4b5e859
CryptoDogg <d.enkikairo@gmail.com>
2022-04-07 15:12:16 +0900

    add property count

--- subgraphs/staking-amount-l2/src/mapping.ts
+++ subgraphs/staking-amount-l2/src/mapping.ts
@@ -3,9 +3,15 @@ import {
   Updated as UpdatedEvent,
 } from "../generated/STokensManager/STokensManager"
 import {
+  MetricsFactory,
+  Create as CreateEvent,
+  Destroy as DestroyEvent,
+} from "../generated/MetricsFactory/MetricsFactory"
+import {
   Lockup
 } from "../generated/STokensManager/Lockup"
 import {
+  PropertyCount,
   TotalAmount,
 } from "../generated/schema"
 
@@ -16,12 +22,12 @@ export function handleMinted(event: MintedEvent): void {
     totalAmount = new TotalAmount(
       day.toString()
     )
-    let lockup = Lockup.bind(event.transaction.to!)
-    let getAllValueResult = lockup.try_totalLocked()
-    if (!getAllValueResult.reverted) {
-      totalAmount.amount = getAllValueResult.value
-      totalAmount.save()
-    }
+  }
+  let lockup = Lockup.bind(event.transaction.to!)
+  let getAllValueResult = lockup.try_totalLocked()
+  if (!getAllValueResult.reverted) {
+    totalAmount.amount = getAllValueResult.value
+    totalAmount.save()
   }
 }
 
@@ -32,11 +38,37 @@ export function handleUpdated(event: UpdatedEvent): void {
     totalAmount = new TotalAmount(
       day.toString()
     )
-    let lockup = Lockup.bind(event.transaction.to!)
-    let getAllValueResult = lockup.try_totalLocked()
-    if (!getAllValueResult.reverted) {
-      totalAmount.amount = getAllValueResult.value
-      totalAmount.save()
-    }
   }
+  let lockup = Lockup.bind(event.transaction.to!)
+  let getAllValueResult = lockup.try_totalLocked()
+  if (!getAllValueResult.reverted) {
+    totalAmount.amount = getAllValueResult.value
+    totalAmount.save()
+  }
+}
+
+export function handleCreate(event: CreateEvent): void {
+  let day = event.block.timestamp.toI32() / 86400
+  let propertyCount = PropertyCount.load(day.toString())
+  if (propertyCount === null) {
+    propertyCount = new PropertyCount(
+      day.toString()
+    )
+  }
+  let metricsFactory = MetricsFactory.bind(event.address);
+  propertyCount.count = metricsFactory.metricsCount()
+  propertyCount.save()
+}
+
+export function handleDestroy(event: DestroyEvent): void {
+  let day = event.block.timestamp.toI32() / 86400
+  let propertyCount = PropertyCount.load(day.toString())
+  if (propertyCount === null) {
+    propertyCount = new PropertyCount(
+      day.toString()
+    )
+  }
+  let metricsFactory = MetricsFactory.bind(event.address);
+  propertyCount.count = metricsFactory.metricsCount()
+  propertyCount.save()
 }
