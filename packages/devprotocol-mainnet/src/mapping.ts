4994b3f198db7f76ad18ca087af626c492225d34
aggre <hiroyuki.aggre@gmail.com>
2022-06-13 10:02:09 +0000

    create linting envs

--- packages/devprotocol-mainnet/src/mapping.ts
+++ packages/devprotocol-mainnet/src/mapping.ts
@@ -1,60 +1,66 @@
+import { Transfer as TransferEvent } from '../generated/DevToken/DevToken'
+import { EternalStorage } from '../generated/DevToken/EternalStorage'
 import {
-  Transfer as TransferEvent
-} from "../generated/DevToken/DevToken"
-import {
-  EternalStorage
-} from "../generated/DevToken/EternalStorage"
-import {
-  TotalLockedAmount,
-  TotalAuthenticatedProperty
-} from "../generated/schema"
-import {
-  Create as CreateEvent
-} from "../generated/MetricsFactory_1/MetricsFactory"
-import { Bytes, Address, BigInt  } from '@graphprotocol/graph-ts'
+	TotalLockedAmount,
+	TotalAuthenticatedProperty,
+} from '../generated/schema'
+import { Create as CreateEvent } from '../generated/MetricsFactory_1/MetricsFactory'
+import { Bytes, Address, BigInt } from '@graphprotocol/graph-ts'
 
 export function handleTransfer(event: TransferEvent): void {
-  let day = event.block.timestamp.toI32() / 86400
-  // totalAmount
-  let totalLockedAmount = TotalLockedAmount.load(day.toString())
-  if (totalLockedAmount === null) {
-    totalLockedAmount = new TotalLockedAmount(
-      day.toString()
-    )
-  }
-  let eternalStorage = EternalStorage.bind(Address.fromString('0x4a154e51b69A798d854E100fDf79e6f0Be0e330D'))
-  let getAllValueResult = eternalStorage.try_getUint(
-    Bytes.fromHexString('0xe7dcf1d83b6e7da9390ea33f3813dc78de09a758d09c9be500b16f152c88c364')
-  )
-  if (!getAllValueResult.reverted) {
-    totalLockedAmount.amount = getAllValueResult.value
-    totalLockedAmount.save()
-  }
+	const day = event.block.timestamp.toI32() / 86400
+	// TotalAmount
+	let totalLockedAmount = TotalLockedAmount.load(day.toString())
+	if (totalLockedAmount === null) {
+		totalLockedAmount = new TotalLockedAmount(day.toString())
+	}
+
+	const eternalStorage = EternalStorage.bind(
+		Address.fromString('0x4a154e51b69A798d854E100fDf79e6f0Be0e330D')
+	)
+	const getAllValueResult = eternalStorage.try_getUint(
+		Bytes.fromHexString(
+			'0xe7dcf1d83b6e7da9390ea33f3813dc78de09a758d09c9be500b16f152c88c364'
+		)
+	)
+	if (!getAllValueResult.reverted) {
+		totalLockedAmount.amount = getAllValueResult.value
+		totalLockedAmount.save()
+	}
 }
 
 export function handleMetricsChanged(event: CreateEvent): void {
-  let day = event.block.timestamp.toI32() / 86400
-  // totalAmount
-  let totalAuthenticatedProperty = TotalAuthenticatedProperty.load(day.toString())
-  if (totalAuthenticatedProperty === null) {
-    totalAuthenticatedProperty = new TotalAuthenticatedProperty(
-      day.toString()
-    )
-  }
-  let eternalStorage = EternalStorage.bind(Address.fromString('0x7F5FC5E49F7eCded3D361EF739619ECb760DcD0b'))
-  let getAuthenticatedProperties = eternalStorage.try_getUint(
-    Bytes.fromHexString('0xd361f1295b0fc1c89cea582568f2b1e03704de9dc1a2ba591754dc3c3f23088a')
-  )
-  if (!getAuthenticatedProperties.reverted && getAuthenticatedProperties.value.notEqual(BigInt.fromI32(0))) {
-    totalAuthenticatedProperty.count = getAuthenticatedProperties.value
-    totalAuthenticatedProperty.save()
-    return
-  }
+	const day = event.block.timestamp.toI32() / 86400
+	// TotalAmount
+	let totalAuthenticatedProperty = TotalAuthenticatedProperty.load(
+		day.toString()
+	)
+	if (totalAuthenticatedProperty === null) {
+		totalAuthenticatedProperty = new TotalAuthenticatedProperty(day.toString())
+	}
 
-  let getTotalIssuedMetrics = eternalStorage.try_getUint(
-    Bytes.fromHexString('0xb2c6b8b2c77e4c24f7c5a750a14d2d8b137567832e6b70ab43d2915c87c8e263')
-  )
-  totalAuthenticatedProperty.count = getTotalIssuedMetrics.value
-  totalAuthenticatedProperty.save()
+	const eternalStorage = EternalStorage.bind(
+		Address.fromString('0x7F5FC5E49F7eCded3D361EF739619ECb760DcD0b')
+	)
+	const getAuthenticatedProperties = eternalStorage.try_getUint(
+		Bytes.fromHexString(
+			'0xd361f1295b0fc1c89cea582568f2b1e03704de9dc1a2ba591754dc3c3f23088a'
+		)
+	)
+	if (
+		!getAuthenticatedProperties.reverted &&
+		getAuthenticatedProperties.value.notEqual(BigInt.fromI32(0))
+	) {
+		totalAuthenticatedProperty.count = getAuthenticatedProperties.value
+		totalAuthenticatedProperty.save()
+		return
+	}
+
+	const getTotalIssuedMetrics = eternalStorage.try_getUint(
+		Bytes.fromHexString(
+			'0xb2c6b8b2c77e4c24f7c5a750a14d2d8b137567832e6b70ab43d2915c87c8e263'
+		)
+	)
+	totalAuthenticatedProperty.count = getTotalIssuedMetrics.value
+	totalAuthenticatedProperty.save()
 }
-
--- packages/devprotocol-v2s/src/mapping.ts
+++ packages/devprotocol-v2s/src/mapping.ts
@@ -1,46 +1,44 @@
 import {
-  Minted as MintedEvent,
-  Updated as UpdatedEvent,
-} from "../generated/STokensManager/STokensManager"
+	Minted as MintedEvent,
+	Updated as UpdatedEvent,
+} from '../generated/STokensManager/STokensManager'
+import { Lockup } from '../generated/STokensManager/Lockup'
 import {
-  Lockup
-} from "../generated/STokensManager/Lockup"
+	TotalLockedAmount,
+	TotalAuthenticatedProperty,
+} from '../generated/schema'
 import {
-  TotalLockedAmount,
-  TotalAuthenticatedProperty
-} from "../generated/schema"
-import {
-  MetricsFactory,
-  Create as CreateEvent
-} from "../generated/MetricsFactory/MetricsFactory"
+	MetricsFactory,
+	Create as CreateEvent,
+} from '../generated/MetricsFactory/MetricsFactory'
 
-export function handleSTokensChanged(event: MintedEvent): void {
-  let day = event.block.timestamp.toI32() / 86400
-  let totalLockedAmount = TotalLockedAmount.load(day.toString())
-  if (totalLockedAmount === null) {
-    totalLockedAmount = new TotalLockedAmount(
-      day.toString()
-    )
-  }
-  let lockup = Lockup.bind(event.transaction.to!)
-  let getAllValueResult = lockup.try_totalLocked()
-  if (!getAllValueResult.reverted) {
-    totalLockedAmount.amount = getAllValueResult.value
-    totalLockedAmount.save()
-  }
+export function handleStokensChanged(event: MintedEvent): void {
+	const day = event.block.timestamp.toI32() / 86400
+	let totalLockedAmount = TotalLockedAmount.load(day.toString())
+	if (totalLockedAmount === null) {
+		totalLockedAmount = new TotalLockedAmount(day.toString())
+	}
+
+	const lockup = Lockup.bind(event.transaction.to!)
+	const getAllValueResult = lockup.try_totalLocked()
+	if (!getAllValueResult.reverted) {
+		totalLockedAmount.amount = getAllValueResult.value
+		totalLockedAmount.save()
+	}
 }
 
 export function handleMetricsChanged(event: CreateEvent): void {
-  let day = event.block.timestamp.toI32() / 86400
-  // totalAmount
-  let totalAuthenticatedProperty = TotalAuthenticatedProperty.load(day.toString())
-  if (totalAuthenticatedProperty === null) {
-    totalAuthenticatedProperty = new TotalAuthenticatedProperty(
-      day.toString()
-    )
-  }
-  let metricsFactory = MetricsFactory.bind(event.address);
-  totalAuthenticatedProperty.count = metricsFactory.authenticatedPropertiesCount();
-  totalAuthenticatedProperty.save()
-}
+	const day = event.block.timestamp.toI32() / 86400
+	// TotalAmount
+	let totalAuthenticatedProperty = TotalAuthenticatedProperty.load(
+		day.toString()
+	)
+	if (totalAuthenticatedProperty === null) {
+		totalAuthenticatedProperty = new TotalAuthenticatedProperty(day.toString())
+	}
 
+	const metricsFactory = MetricsFactory.bind(event.address)
+	totalAuthenticatedProperty.count =
+		metricsFactory.authenticatedPropertiesCount()
+	totalAuthenticatedProperty.save()
+}
